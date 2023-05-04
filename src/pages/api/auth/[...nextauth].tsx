54af16cf219accd17118f643a1dee7d335ba33d9
kaiser24 <damsog38@gmail.com>
2023-05-04 16:21:08 -0500

    Moved api from app to pages directory. apparently it doesn't work properly yet

---
+++ src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,44 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "../../../lib/prisma";
+
+export const authOptions:NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.uid = user.id;
+        token.email = user.email;
+        //token.apikey = user.apiKey;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        //session.apikey = token.apikey;
+        //session.userId = token.uid;
+        //session.userEmail = token.email;
+        //console.log('data: ' + JSON.stringify(session.apikey));
+      }
+      return session
+    }
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    maxAge: 60 * 60, // 1 hour
+  },
+}
+
+export default NextAuth(authOptions)
